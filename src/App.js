e6daae37cb9c5f3f818633e5245e818312b129e0
grigoris24 <grigoris_24@hotmail.com>
2025-01-14 16:49:23 +0200

    Created a background

--- src/App.js
+++ src/App.js
@@ -1,23 +1,80 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+// import MyName from './Components/Myame';
+
+const getRandomColor = () => {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+};
 
 function App() {
+  const [balls, setBalls] = useState([]);
+
+  const randomNumber = (min, max) => Math.random() * (max - min) + min;
+
+  
+
+  const generateBalls = () => {
+    const newBalls = [];
+    for (let i = 0; i < 10; i++) {
+      newBalls.push({
+        id: i,
+        size: randomNumber(30, 100),
+        left: randomNumber(0, window.innerWidth - 100), 
+        top: randomNumber(0, window.innerHeight - 100),
+        speedX: randomNumber(1, 5),
+        speedY: randomNumber(1, 5),
+        color: getRandomColor(),
+      });
+    }
+    setBalls(newBalls);
+  };
+
+  useEffect(() => {
+    generateBalls();
+
+    const interval = setInterval(() => {
+      setBalls((prevBalls) =>
+        prevBalls.map((ball) => ({
+          ...ball,
+          left: ball.left + ball.speedX > window.innerWidth
+            ? -ball.size
+            : ball.left + ball.speedX < -ball.size
+            ? window.innerWidth
+            : ball.left + ball.speedX,
+          
+          top: ball.top + ball.speedY > window.innerHeight
+            ? -ball.size
+            : ball.top + ball.speedY < -ball.size
+            ? window.innerHeight
+            : ball.top + ball.speedY,
+        }))
+      );
+    }, 16);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {balls.map((ball) => (
+        <div
+          key={ball.id}
+          className="ball"
+          style={{
+            width: ball.size + 'px',
+            height: ball.size + 'px',
+            left: ball.left + 'px',
+            top: ball.top + 'px',
+            backgroundColor: ball.color,
+          }}
+        ></div>
+      ))}
+      {/* <MyName /> */}
     </div>
   );
 }
---
+++ src/Components/MyName.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './MyName.css';
+
+function MyName() {
+  return (
+    <div>
+      <h1>Grigoris Papadopoulos</h1>
+    </div>
+  );
+}
+
+export default MyName;
